f702f1ecb88bdff0e3aa8b4ec11dbd460f18fa14
John Sattari <johnsattari@johns-air.lan>
2022-02-06 16:39:28 -0800

    added OOP struct with a single method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use std::collections::HashMap;
+
 fn main() {
+
+    // code to get first two arguments in console
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
     println!("{:?}, {:?}", action, item);
+}
+
+struct Todo {
+    // implement hashmap
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // we pass true as value
+        self.map.insert(key, true);
+    }
 }
