b1d8784a959c313a3f6945d54d67776ed08b4212
John Sattari <johnsattari@Johns-MacBook-Air.local>
2022-02-06 22:51:07 -0800

    added two new methods to impl Todo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 
 fn main() {
-
     // code to get first two arguments in console
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
@@ -19,5 +18,21 @@ impl Todo {
         // insert a new item into our map.
         // we pass true as value
         self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+
+        // create content variable as String
+        let mut content = String::new();
+
+        // loop through hashmap
+        // format as String with newlines
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+
+        // save content variable as txt file on disk
+        std::fs::write("db.txt", content)
     }
 }
