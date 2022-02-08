3ed2215617d8ad5da3cf71446b55839401d9b7e4
John Sattari <johnsattari@johns-air.lan>
2022-02-07 22:42:05 -0800

    Added new method and updated main function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::collections::HashMap;
+use std::io::Read;
+use std::str::FromStr;
 
 fn main() {
     // code to get first two arguments in console
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?}, {:?}", action, item);
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
 
 struct Todo {
@@ -14,6 +26,23 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         // insert a new item into our map.
         // we pass true as value
@@ -21,7 +50,6 @@ impl Todo {
     }
 
     fn save(self) -> Result<(), std::io::Error> {
-
         // create content variable as String
         let mut content = String::new();
 
